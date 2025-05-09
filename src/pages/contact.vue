471976fa8e389c57278aa7d1bc27b23599ce64ea
Kachenas <almonte.s.chester@gmail.com>
2025-05-09 18:40:43 +0800

    first commit

---
+++ src/pages/contact.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="left-col">
+      <div class="flex-item item-1">Item 1</div>
+      <div class="flex-item item-2">Item 2</div>
+    </div>
+    <div class="center-col">
+      <div class="flex-item item-3">Item 3</div>
+    </div>
+    <div class="right-col">
+      <div class="flex-item item-4">Item 4</div>
+      <div class="flex-item item-5">Item 5</div>
+    </div>
+  </div>
+</template>
+<style lang="css" scoped>
+body {
+    margin: 0;
+}
+
+.container {
+    border: 6px solid black;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.left-col,
+.center-col,
+.right-col {
+    display: flex;
+    gap: 0.5rem;
+}
+
+.left-col,
+.right-col {
+    flex-shrink: 0;
+}
+
+.center-col {
+  justify-content: center;
+  flex: 2; /* allows it to stay centered */
+}
+
+.flex-item {
+    color: white;
+    font-size: 1.5rem;
+    padding: 1rem;
+    text-align: center;
+}
+
+.item-1 {
+    background-color: orange;
+}
+.item-2 {
+    background-color: red;
+}
+.item-3 {
+    background-color: green;
+    width: 100%;
+    max-width: 300px;
+}
+.item-4 {
+    background-color: blue;
+}
+.item-5 {
+    background-color: yellow;
+}
+</style>
---
+++ src/pages/profile.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="color in colors" :key="color">
+        <div class="sample" :style="{ backgroundColor: color }">
+          sample
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" lg="6" md="6" sm="6">
+        <v-text-field clearable label="First Name" prepend-icon="$vuetify" variant="solo-filled" />
+      </v-col>
+      <v-col cols="12" lg="6" md="6" sm="6">
+        <v-text-field clearable label="Last Name" prepend-icon="$vuetify" variant="solo-filled" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" lg="6" md="6" sm="6">
+        <v-text-field clearable label="Email" prepend-icon="$vuetify" type="email" variant="solo-filled" />
+      </v-col>
+      <v-col cols="12" lg="6" md="6" sm="6">
+        <v-text-field clearable label="Last Name" prepend-icon="$vuetify" variant="solo-filled" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  const colors = [
+    'red', 'yellow', 'white', 'green', 'blue',
+    'violet', 'pink', 'brown', 'orange', 'teal',
+    'cyan', 'aquamarine',
+  ];
+</script>
+
+<style scoped>
+.sample {
+  height: 100px;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+  border: 1px solid #ccc;
+  color: black;
+  padding: 8px;
+  text-align: center;
+  word-wrap: break-word;     /* allow long words to wrap */
+  overflow-wrap: break-word; /* modern equivalent for better support */
+  white-space: normal;       /* allows wrapping */
+}
+
+</style>
